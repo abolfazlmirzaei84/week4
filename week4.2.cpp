1d0acb6daa6022247c0b719bcea237ff2c68b04c
abolfazlmirzaei84 <152904559+abolfazlmirzaei84@users.noreply.github.com>
2023-12-04 20:40:42 +0330

    Add files via upload

---
+++ week4.2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<stdlib.h>
+#include <math.h>
+using namespace std;
+
+int main() {
+  cout << "please enter three integer number"<<"\n";
+  int x,y,z;
+  cin>>x>>y>>z;
+  
+if(((x+y+z)*(x+y+z))-1 == 0){
+cout<<"invalid numbers (The root of the denominator of the equation becomes zero)";
+}else{
+	float answer = static_cast<float>((x-y)*(x+z))/(static_cast<float>(((x+y+z)*(x+y+z))+1));
+	cout<<"(x-y)*(x+z)"<<"\n"<<"-----------"<<"\n"<<"(x+y+z)^2+1";
+	cout<<"\n"<<"the answer is :"<<answer;
+}
+ getchar();
+}
